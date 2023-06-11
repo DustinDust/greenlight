c4ff5314024d3f8467937f6a88f70ac1c81117c0
NamDang25 <79992369+DustinDust@users.noreply.github.com>
2023-06-11 11:10:37 +0000

    Added validator class

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -8,6 +8,7 @@ import (
 	"io"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/julienschmidt/httprouter"
 )
@@ -41,7 +42,12 @@ func (app *application) writeJSONResponse(w http.ResponseWriter, status int, dat
 
 // Unmarshal request JSON body into object & handling error
 func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst interface{}) error {
-	err := json.NewDecoder(r.Body).Decode(dst)
+	// 1Mb of maximum bytes
+	maxSize := 1_048_576
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxSize))
+	dec := json.NewDecoder(r.Body)
+	dec.DisallowUnknownFields()
+	err := dec.Decode(dst)
 	if err != nil {
 		var syntaxError *json.SyntaxError
 		var unmarshalTypeError *json.UnmarshalTypeError
@@ -62,13 +68,20 @@ func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst int
 		case errors.Is(err, io.EOF):
 			return errors.New(constant.JSON_EMPTY)
 
+		case strings.HasPrefix(err.Error(), "json: unknown field "):
+			fieldName := strings.TrimPrefix(err.Error(), "json: unknown field ")
+			return fmt.Errorf("body contains unknown key %s", fieldName)
+
+		case err.Error() == "http: request body too large":
+			return fmt.Errorf("body must not be larger than %d bytes", maxSize)
+
 		case errors.As(err, &invalidUnmarshalError):
 			panic(err)
 
 		default:
 			return err
 		}
-
+		
 	}
 	return nil
 }
--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -1,8 +1,10 @@
 package data
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
+	"strings"
 )
 
 type Runtime int32
@@ -11,4 +13,28 @@ func (r Runtime) MarshalJSON() ([]byte, error) {
 	jsonValue := fmt.Sprintf("%d minutes", r)
 	quotedJSONVaue := strconv.Quote(jsonValue)
 	return []byte(quotedJSONVaue), nil
+}
+
+// Custom unmarshalling runtime fields.
+func (r *Runtime) UnmarshalJSON(jsonValue []byte) error {
+	// check the format ( "__ minutes" )
+	unquotedValue, err := strconv.Unquote(string(jsonValue))
+	if err != nil {
+		return errors.New("invalid runtime format")
+	}
+
+	parts := strings.Split(unquotedValue, " ")
+	if len(parts) != 2 || parts[1] != "minutes" {
+		return errors.New("invalid runtime format")
+	}
+
+	// fail to convert the value back to int32
+	i, err := strconv.ParseInt(parts[0], 10, 32)
+	if err != nil {
+		return errors.New("invalid runtime format")
+	}
+
+	// set the runtime value to the unmarshal on through pointer dereference
+	*r = Runtime(i)
+	return nil
 }
